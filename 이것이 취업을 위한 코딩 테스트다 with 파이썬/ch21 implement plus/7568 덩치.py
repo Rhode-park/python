f0cd7199a627323840b8a7a4832c3744555044e1
Rhode-park <jinah.park.16@gmail.com>
2022-12-07 14:03:48 +0900

    python study 221207 14:03

--- 이것이 취업을 위한 코딩 테스트다 with 파이썬/ch21 implement plus/7568 덩치.py
+++ 이것이 취업을 위한 코딩 테스트다 with 파이썬/ch21 implement plus/7568 덩치.py
@@ -1,5 +1,32 @@
 # https://www.acmicpc.net/problem/7568
 
+# n = int(input())
+#
+# data = []
+#
+# for i in range(n):
+#     data.append(list(map(int, input().split())))
+#
+# scores = [1] * n
+#
+# for j in range(n):
+#     for k in range(n):
+#         if data[j][0] < data[k][0] and data[j][1] < data[k][1]:
+#             scores[j] += 1
+#
+# rank = [n+1] * n
+#
+# for l in range(n):
+#     for m in range(n):
+#         if scores[l] >= scores[m]:
+#             rank[l] -= 1
+#
+#
+# print(*rank, sep= ' ')
+
+# 틀렸음, 게다가 for문을 너무 많이 쓴 것 같음
+# 예시는 잘 돌아가는데.. 왜지...??
+
 n = int(input())
 
 data = []
@@ -7,24 +34,13 @@ data = []
 for i in range(n):
     data.append(list(map(int, input().split())))
 
-scores = [0] * n
+scores = [1] * n
 
 for j in range(n):
     for k in range(n):
-        if data[j][0] > data[k][0]:
-            if data[j][1] > data[k][1]:
-                scores[j] += 1
-
-rank = [n+1] * n
-
-for l in range(n):
-    for m in range(n):
-        if scores[l] >= scores[m]:
-            rank[l] -= 1
+        if data[j][0] < data[k][0] and data[j][1] < data[k][1]:
+            scores[j] += 1
 
 
-print(*rank, sep= ' ')
-
-# 틀렸음, 게다가 for문을 너무 많이 쓴 것 같음
-# 예시는 잘 돌아가는데.. 왜지...??
+print(*scores, sep= ' ')
 
