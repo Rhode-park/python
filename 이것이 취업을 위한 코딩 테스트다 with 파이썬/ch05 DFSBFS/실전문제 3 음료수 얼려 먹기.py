0404640ab5422522e10c4b55ca825b3f6a3cb32b
Rhode-park <jinah.park.16@gmail.com>
2022-12-09 17:24:50 +0900

    python study 221209 17:24

--- 이것이 취업을 위한 코딩 테스트다 with 파이썬/ch05 DFSBFS/실전문제 3 음료수 얼려 먹기.py
+++ 이것이 취업을 위한 코딩 테스트다 with 파이썬/ch05 DFSBFS/실전문제 3 음료수 얼려 먹기.py
@@ -2,9 +2,25 @@ n, m = map(int, input().split())
 
 graph = []
 for i in range(n):
-    graph.append(list(map(int, input().split())))
+    graph.append(list(map(int, input())))
 
 def dfs(x,y):
-    if x<1 or x>n or y<1 or y>m:
+    if x<0 or x>=n or y<0 or y>=m:  # 종료조건1
         return False
-    
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+        dfs(x+1,y)
+        dfs(x-1,y)
+        dfs(x,y+1)
+        dfs(x,y-1)
+        return True
+    return False  # 종료조건2 : 근데 왜 쓰는지 모르겠음.. graph[x][y] != 0일 경우에 False를 return한다는걸까..?
+
+result = 0
+
+for i in range(n):
+    for j in range(m):
+        if dfs(i,j) == True:
+            result += 1
+
+print(result)
---
+++ 이것이 취업을 위한 코딩 테스트다 with 파이썬/ch05 DFSBFS/실전문제 4 미로 탈출.py
@@ -0,0 +1,9 @@
+
+def a():
+    for i in range(10):
+        if i%2 == 0:
+            return True
+        return False
+
+if a() == True:
+    print('a')
