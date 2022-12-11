a56ac8587fffe31e702ac44dc078c19029de8f11
Rhode-park <jinah.park.16@gmail.com>
2022-12-11 19:53:58 +0900

    python study 221211 19:53

---
+++ 이것이 취업을 위한 코딩 테스트다 with 파이썬/ch13 DFSBFS/18 괄호 변환.py
@@ -0,0 +1,33 @@
+# https://programmers.co.kr/learn/courses/30/lessons/60058
+
+# 문제부터 이해하기 어려웠다
+
+p = input()
+
+def checkCorrect(x):  # 이해가 안 됨
+    count = 0
+    for i in range(len(x)-1):
+        if x[i] == '(':
+            count += 1
+            print(i, count)
+        elif x[i] == ')':
+            if count == 0:
+                return False
+            count -= 1
+            print(i, count)
+    return True
+
+
+def devide(x):
+    for i in range(2, len(x), 2):
+        u = p[0:i]
+        v = p[i:len(p)]
+        print(u, v)
+
+
+if checkCorrect(p) == True:
+    print('True')
+
+devide(p)
+
+print(p.count('('))
