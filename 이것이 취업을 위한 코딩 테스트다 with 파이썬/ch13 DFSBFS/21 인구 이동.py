eef9aa92af0d7470d3c1cc3fd643f00f2ecdbddd
Rhode-park <jinah.park.16@gmail.com>
2022-12-17 21:43:01 +0900

    python study 221217 21:42

--- 이것이 취업을 위한 코딩 테스트다 with 파이썬/ch13 DFSBFS/21 인구 이동.py
+++ 이것이 취업을 위한 코딩 테스트다 with 파이썬/ch13 DFSBFS/21 인구 이동.py
@@ -1,12 +1,56 @@
 # https://www.acmicpc.net/problem/16234
 
+# 정답풀이
+
+from collections import deque
+
 n, l, r = map(int, input().split())
 
-A = []
+graph = []
 for i in range(n):
-    A.append(list(map(int, input().split())))
+    graph.append(list(map(int, input().split())))
 
-land = A
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
 
-def dfs():
-    if 
+result = 0
+
+def process(x,y,index):
+    united = []
+    united.append((x,y))
+    queue = deque()
+    queue.append((x,y))
+    union[x][y] = index
+    summary = graph[x][y]
+    count = 1
+    while queue:
+        x,y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0<=nx and nx<n and 0<=ny and ny<n and union[nx][ny] == -1:
+                if l <= abs(graph[nx][ny] - graph[x][y]) <= r:
+                    queue.append((nx,ny))
+                    union[nx][ny] = index
+                    summary += graph[nx][ny]
+                    count += 1
+                    united.append((nx,ny))
+    for i, j in united:
+        graph[i][j] = summary//count
+    return count
+
+totalCount = 0
+
+while True:
+    union = [[-1]*n for _ in range(n)]
+    index = 0
+    for i in range(n):
+        for j in range(n):
+            if union[i][j] == -1:
+                process(i,j,index)
+                index += 1
+    if index == n*n:
+        break
+    totalCount += 1
+
+print(totalCount)
